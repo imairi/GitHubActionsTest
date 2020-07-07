74425a32f0c6c3519774c334ecb56e11b83c7b3c
imairi <imairi@gmail.com>
2020-07-07 17:43:18 +0900

    add space and line break

--- GitHubActionsTest/ContentView.swift
+++ GitHubActionsTest/ContentView.swift
@@ -14,8 +14,12 @@ struct ContentView: View {
     }
 }
 
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+              ContentView()
     }
 }
