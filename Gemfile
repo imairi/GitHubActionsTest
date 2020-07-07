8b583f338bf33213dcdc07330353463e45df19e9
imairi <imairi@gmail.com>
2020-07-07 10:21:24 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem 'cocoapods', '~> 1.8.1'
+gem 'fastlane', '~> 2.23'
+gem 'danger'
+gem 'danger-detect_unused_definition', :github => 'imairi/danger-detect_unused_definition'
---
+++ GitHubActionsTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubActionsTest
+//
+//  Created by 今入　庸介 on 2020/07/07.
+//  Copyright © 2020 今入　庸介. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
