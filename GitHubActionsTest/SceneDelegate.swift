0fb6fd33f85df5df8019ef317c67c5f8ccba09c2
imairi <imairi@gmail.com>
2020-07-07 14:45:49 +0900

    add dummy struct

--- GitHubActionsTest/SceneDelegate.swift
+++ GitHubActionsTest/SceneDelegate.swift
@@ -9,6 +9,10 @@
 import UIKit
 import SwiftUI
 
+struct Test {
+    let test: Int
+}
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -57,5 +61,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
+    
+    private func test() {
+        // nothing.
+    }
 }
